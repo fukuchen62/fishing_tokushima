ead32fee79ee49cafd8d0556726503a4c4e18c3a
noboru-ogawa <no.ogawa560211@gmail.com>
2023-02-08 18:16:19 +0900

    Update UsersTableSeeder.php
    usersのシーダーファイルの修正
    
    小川

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,38 +17,35 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $param=[
-            'id' => 1,
+
             'name'=> '山田　太郎',
             'nickname'=> 'やまちゃん',
             'email'=> 'tamada_t@gmail.com',
-            'email_verified_at'=> 'tamada_t@gmail.com',
             'password'=> 12345678,
             'authority'=> 3,
-            'is_show'=> 1,
+            'is_show'=> true,
         ];
         DB::table('users') -> insert($param);
 
         $param=[
-            'id' => 2,
+
             'name'=> '徳島　太郎',
             'nickname'=> 'やまちゃん',
             'email'=> 'kokushima_t@gmail.com',
-            'email_verified_at'=> 'tokushima_t@gmail.com',
             'password'=> 12345678,
             'authority'=> 2,
-            'is_show'=> 1,
+            'is_show'=>  true,
         ];
         DB::table('users') -> insert($param);
 
         $param=[
-            'id' => 3,
+
             'name'=> '徳島　次郎',
             'nickname'=> '次郎',
             'email'=> 'kokushima_j@gmail.com',
-            'email_verified_at'=> 'tokushima_j@gmail.com',
             'password'=> 12345678,
             'authority'=> 1,
-            'is_show'=> 1,
+            'is_show'=>  true,
         ];
         DB::table('users') -> insert($param);
     }
