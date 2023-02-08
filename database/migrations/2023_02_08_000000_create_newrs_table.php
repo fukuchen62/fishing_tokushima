9263b1e13e7d509641889513557e5ec6f65cdc7f
noboru-ogawa <no.ogawa560211@gmail.com>
2023-02-08 15:38:11 +0900

    マイグレーション作成
    新規作成
    
    小川

---
+++ database/migrations/2023_02_08_000000_create_newrs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',10)->nullable(false);
+            $table->string('title',50)->nullable(false);
+            $table->string('overview',150)->nullable(false);
+            $table->text('content')->nullable(false);
+            $table->string('eyecatch',200);
+            $table->boolean('is_show')->default(true);
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+};
