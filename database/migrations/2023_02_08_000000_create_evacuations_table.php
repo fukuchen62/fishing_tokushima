e2deee2a4930e158023f4f0319123efc14df36ac
shimohachi-ryotaro <124222136+shimohachi-ryotaro@users.noreply.github.com>
2023-02-08 15:37:49 +0900

    マイグレーション作成
    evacuationsテーブルのマイグレーションを追加
    
    下八

---
+++ database/migrations/2023_02_08_000000_create_evacuations_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evacuations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',50)->nullable(false);
+            $table->string('city_id',20)->nullable(false);
+            $table->string('address',200)->nullable(false);
+            $table->string('longitude',20);
+            $table->string('latitude',20);
+            $table->string('img1',200);
+            $table->string('img2',200);
+            $table->string('img3',200);
+            $table->string('img4',200);
+            $table->string('img5',200);
+            $table->string('img6',200);
+            $table->string('img7',200);
+            $table->string('img8',200);
+            $table->string('img9',200);
+            $table->string('img9',200);
+            $table->string('tel',20);
+            $table->string('classification',20);
+            $table->string('disaster',200);
+            $table->string('memo',200);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uevacuations');
+    }
+};
