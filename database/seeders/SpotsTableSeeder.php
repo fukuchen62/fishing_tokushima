dc07a65e116f66217f5816ed651f49eeee6902e9
panamochi <dalkur3@gmail.com>
2023-02-08 23:48:25 +0900

    スポット用テストデータ
    スポット用のテストデータシーダ

---
+++ database/seeders/SpotsTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+
+
+class SpotsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => '岡崎海岸',
+            'overview' => 'テストです',
+            'city_id' => 3,
+            'postal_code' => '772-0013',
+            'spot_address' => '徳島県鳴門市撫養町岡崎二等道路東-88',
+            'iframe_url' => 'test',
+            'toilet' => '有',
+            'created_at' => date('Y-m-d H:i:s'),
+        ];
+        DB::table('spots')->insert($param);
+
+        $param = [
+            'name' => '亀浦観光港',
+            'overview' => 'テスト2です',
+            'city_id' => 3,
+            'postal_code' => '772-0053',
+            'spot_address' => '徳島県鳴門市鳴門町土佐泊浦大毛　亀浦漁港',
+            'iframe_url' => 'test',
+            'toilet' => '有',
+            'created_at' => date('Y-m-d H:i:s'),
+        ];
+        DB::table('spots')->insert($param);
+    }
+}
