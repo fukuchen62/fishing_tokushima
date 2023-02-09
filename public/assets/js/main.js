d1015aafde820ab4bcaeaa493ea1e946fbe41309
Hhamada1 <124231052+Hhamada1@users.noreply.github.com>
2023-02-09 16:16:26 +0900

    Update main.js
    main.jsを修正し、メニューが正しく表示されない問題を解決しました。
    
    濱田大貴

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,4 @@
 $(".hamburger").click(function () {
     $(this).toggleClass('active');
-    $(".sidemenu").toggleClass('sidemenu__slide');
-    // $(".logo__img").toggleClass('none');
     $(".header__box").toggleClass('sidemenu__slide');
 });
