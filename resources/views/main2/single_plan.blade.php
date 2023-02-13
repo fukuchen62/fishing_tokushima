0732c42ee7276573b1890b439a9123f217c7ecf5
toshikawa-yurika <124222099+toshikawa-yurika@users.noreply.github.com>
2023-02-13 12:15:18 +0900

    plan関連を修正
    plan関連を修正

--- resources/views/main2/single_plan.blade.php
+++ resources/views/main2/single_plan.blade.php
@@ -7,6 +7,31 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="./single_plan" method="post">
+        @csrf
+        <input type="text" name="input" value="{{ $input }}">
+        <input type="submit" value="find">
+    </form>
+
+    @if (isset($item))
+        <table>
+            <tr>
+                <th>title</th>
+                <th>overview</th>
+                <th>level</th>
+                <th>flow</th>
+                <th>thumbnail</th>
+            </tr>
+            <tr>
+                <td>{{ $item -> title }}</td>
+                <td>{{ $item -> thumbnail }}</td>
+                <td>{{ $item -> overview }}</td>
+                <td>{{ $item -> level }}</td>
+                <td>{{ $item -> flow }}</td>
+            </tr>
+        </table>
+    @endif
+
 
 </body>
 </html>
