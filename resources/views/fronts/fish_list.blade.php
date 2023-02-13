ac3331bfb28f0372470229c0ceabffb1ec06f239
OkadaNaho <mottu0401@gmail.com>
2023-02-13 16:14:34 +0900

    Update fish_list.blade.php
    fish_list.blade.phpを編集
    
    岡田

--- resources/views/fronts/fish_list.blade.php
+++ resources/views/fronts/fish_list.blade.php
@@ -12,14 +12,29 @@
     <h1>徳島で釣れる魚一覧</h1>
     <table>
         <tr>
-            <th>魚一覧</th>
+            <th>魚の名前(普通の呼び方)</th>
+            <th>魚の名前(正式名称)</th>
+            <th>魚の画像1</th>
+            <th>詳細説明</th>
+            <th>食べ方1</th>
+            <th>食べ方2</th>
+            <th>食べ方3</th>
+            <th>リンク</th>
         </tr>
-        @foreach($fishes as $item)
-        <tr>
-            <td>{{$item->getData()}}</td>
-        </tr>
+        @foreach ($fishes as $item)
+            <tr>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->formal_name }}</td>
+                <td>{{ $item->fish_img1 }}</td>
+                <td>{{ $item->detail }}</td>
+                <td>{{ $item->cooking1 }}</td>
+                <td>{{ $item->cooking2 }}</td>
+                <td>{{ $item->cooking3 }}</td>
+                <td><a href="./fish_info?id={{ $item->id }}">詳細ページへ</a></td>
+            </tr>
         @endforeach
     </table>
+
 </body>
 
 </html>
