32766e08490af0b6b2945eecda9dc0f57aea54a3
noboru-ogawa <no.ogawa560211@gmail.com>
2023-02-14 14:19:11 +0900

    Update News.php
    修正
    
    小川

--- app/Models/News.php
+++ app/Models/News.php
@@ -12,8 +12,19 @@ class News extends Model
 
     public static $rules = array(
         'name' => 'required',
-        'title' => 'required'
+        'title' => 'required',
+        'overview' => 'required',
+        'content' => 'required'
     );
+
+// 日本語エラーメッセージ
+    public static $messages = [
+        'name.required' => 'タイプは必ず入力してください。',
+        'title.required' => 'タイトルは必ず入力してください。',
+        'overview.required' => '概要は必ず入力してください。',
+        'content.required' => '記事内容は必ず入力してください。',
+
+];
 
     public function getData()
     {
