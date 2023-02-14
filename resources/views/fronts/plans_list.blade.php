7ca5d936788c54f4c1c100adec87ed32dffd32bd
toshikawa-yurika <124222099+toshikawa-yurika@users.noreply.github.com>
2023-02-14 15:52:55 +0900

    plan関連修正
    plan関連修正
    
    歳川

--- resources/views/fronts/plans_list.blade.php
+++ resources/views/fronts/plans_list.blade.php
@@ -11,62 +11,59 @@
 <body>
     <h1>体験プランの一覧</h1>
 
-    <form action="./planlist" method="post">
+    {{-- <form action="./list" method="post">
         @csrf
-        <input type="number" name="area" value="">
-        {{-- {{ $area }} --}}
-        <input type="text" name="level" value="">
-        {{-- {{ $level }} --}}
-        <input type="text" name="fish" value="">
-        {{-- {{ $fish }} --}}
+        <input type="number" name="area" value="{{ $area }}">
+        <input type="text" name="level" value="{{ $level }}">
+        <input type="text" name="fish" value="{{ $fish }}">
         <input type="submit" value="find">
-    </form>
+    </form> --}}
 
-
-    <form action="./planlist" method="post">
+    <form action="{{ route('planlist')}}" method="post">
         @csrf
 
         {{-- area --}}
         <div class="checklist">
-            <input type="checkbox" name="area" value="1" id="toubu" onclick="" /><label for="toubu">東部</label>
+            <input type="checkbox" name="area[]" value="1" id="toubu" onclick="" /><label for="toubu">東部</label>
         </div>
 
         <div class="checklist">
-            <input type="checkbox" name="area" value="2" id="nanbu" onclick="" /><label for="nanbu">南部</label>
+            <input type="checkbox" name="area[]" value="2" id="nanbu" onclick="" /><label for="nanbu">南部</label>
         </div>
 
         <div class="checklist">
-            <input type="checkbox" name="area" value="3" id="seibu" onclick="" disabled=true /><label for="seibu">西部</label>
+            <input type="checkbox" name="area[]" value="3" id="seibu" onclick="" disabled=true /><label for="seibu">西部</label>
         </div>
 
         {{-- level --}}
         <div class="checklist">
-            <input type="checkbox" name="level" value="1" id="toubu" onclick="" /><label for="toubu">★</label>
+            <input type="checkbox" name="level[]" value="1" id="toubu" onclick="" /><label for="toubu">★</label>
         </div>
 
         <div class="checklist">
-            <input type="checkbox" name="level" value="2" id="nanbu" onclick="" /><label for="nanbu">★★</label>
+            <input type="checkbox" name="level[]" value="2" id="nanbu" onclick="" /><label for="nanbu">★★</label>
         </div>
 
         <div class="checklist">
-            <input type="checkbox" name="level" value="3" id="seibu" onclick="" /><label for="seibu">★★★</label>
+            <input type="checkbox" name="level[]" value="3" id="seibu" onclick="" /><label for="seibu">★★★</label>
         </div>
 
         {{-- fish --}}
         <div class="checklist">
-            <input type="checkbox" name="fish" value="1" id="toubu" onclick="" /><label for="toubu">アジ</label>
+            <input type="checkbox" name="fish[]" value="1" id="toubu" onclick="" /><label for="toubu">アジ</label>
         </div>
 
         <div class="checklist">
-            <input type="checkbox" name="fish" value="2" id="nanbu" onclick="" /><label for="nanbu">イワシ</label>
+            <input type="checkbox" name="fish[]" value="2" id="nanbu" onclick="" /><label for="nanbu">イワシ</label>
         </div>
 
         <input type="submit" value="find">
     </form>
 
-
-    <table>
+    @if ($plans!=null)
+        <table>
         <tr>
+            <th>ID</th>
             <th>アイキャッチ</th>
             <th>体験プラン</th>
             <th>概要</th>
@@ -74,13 +71,17 @@
         </tr>
         @foreach ($plans as $item)
             <tr>
+                <td>{{ $item->id }}</td>
                 <td>{{ $item->eye_catch }}</td>
                 <td>{{ $item->title }}</td>
                 <td>{{ $item->overview }}</td>
-                <td><a href="./plans_info?id={{ $item->id }}">リンク</a></td>
+                <td><a href="{{ route('planinfo', ['id' =>  $item->id ]) }}">リンク</a></td>
             </tr>
         @endforeach
     </table>
+    @endif
+
+
 
 </body>
 
