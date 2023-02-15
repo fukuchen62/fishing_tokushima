efbc11fb95e56ed27a1692a1e3eaa6dbfd6f93b9
OkadaNaho <mottu0401@gmail.com>
2023-02-15 14:44:09 +0900

    fish関連ファイルの編集
    fish関連ファイルの編集
    
    岡田

--- resources/views/fronts/fish_info.blade.php
+++ resources/views/fronts/fish_info.blade.php
@@ -10,45 +10,42 @@
 
 <body>
 
-    @if (isset($item))
-        <table>
-            <tr>
-                <th>魚の名前(普通の呼び方)</th>
-                <th>魚の名前(正式名称)</th>
-                <th>魚の画像1</th>
-                <th>詳細説明</th>
-                <th>難易度</th>
-                <th>釣れる月</th>
-                <th>釣れる場所</th>
-                <th>釣り方</th>
-                <th>食べ方1</th>
-                <th>食べ方2</th>
-                <th>食べ方3</th>
-                <th>食べ方の写真1</th>
-                <th>注意事項</th>
-            </tr>
-            <tr>
-                <td>{{ $item->name }}</td>
-                <td>{{ $item->formal_name }}</td>
-                <td>{{ $item->fish_img1 }}</td>
-                <td>{{ $item->detail }}</td>
-                <td>{{ $item->level }}</td>
-                <td>{{ $item->getMonth() }}</td>
-                <td>
-                    @foreach ($spots as $spot)
-                        {{ $spot->name }},
-                    @endforeach
-
-                </td>
-                <td>{{ $item->method }}</td>
-                <td>{{ $item->cooking1 }}</td>
-                <td>{{ $item->cooking2 }}</td>
-                <td>{{ $item->cooking3 }}</td>
-                <td>{{ $item->cooking_img1 }}</td>
-                <td>{{ $item->warning }}</td>
-            </tr>
-        </table>
-    @endif
+    <table>
+        <tr>
+            <th>魚の名前(普通の呼び方)</th>
+            <th>魚の名前(正式名称)</th>
+            <th>魚の画像1</th>
+            <th>詳細説明</th>
+            <th>難易度</th>
+            <th>釣れる月</th>
+            <th>釣れる場所</th>
+            <th>釣り方</th>
+            <th>食べ方1</th>
+            <th>食べ方2</th>
+            <th>食べ方3</th>
+            <th>食べ方の写真1</th>
+            <th>注意事項</th>
+        </tr>
+        <tr>
+            <td>{{ $item->name }}</td>
+            <td>{{ $item->formal_name }}</td>
+            <td>{{ $item->fish_img1 }}</td>
+            <td>{{ $item->detail }}</td>
+            <td>{{ $item->level }}</td>
+            <td>{{ $item->getMonth() }}</td>
+            <td>
+                @foreach ($spots as $spot)
+                    {{ $spot->name }},
+                @endforeach
+            </td>
+            <td>{{ $item->method }}</td>
+            <td>{{ $item->cooking1 }}</td>
+            <td>{{ $item->cooking2 }}</td>
+            <td>{{ $item->cooking3 }}</td>
+            <td>{{ $item->cooking_img1 }}</td>
+            <td>{{ $item->warning }}</td>
+        </tr>
+    </table>
 
     <ul>
         @foreach ($fishlist as $fish)
