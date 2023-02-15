05d0c8afb4301a8fdd1b0525e6f776f091e012bf
shimohachi-ryotaro <124222136+shimohachi-ryotaro@users.noreply.github.com>
2023-02-15 10:54:34 +0900

    Update AdminController.php
    admin編集
    
    下八

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,6 +15,8 @@ use App\Models\Fish;
 
 use App\Models\Spot;
 
+use App\Models\Plan;
+
 
 // DBクラスをインポートする
 use Illuminate\Support\Facades\DB;
@@ -309,4 +311,93 @@ class AdminController extends Controller
         Fish::find($request->id)->delete();
         return redirect()->route('fishshow');
     }
+
+    //plan分
+
+    public function planSearch(Request $request)
+    {
+        // クライアントから検索条件(s)を取得する
+        $s = "";
+        if (isset($request->s)) {
+            $s = $request->s;
+        }
+
+        if ($s !== '') {
+            // あいまい検索
+            $items = DB::table('people')
+                ->where('id', 'like', '%' . $s . "%")
+                ->orwhere('name', 'like', '%' . $s . "%")
+                ->orWhere('sex', 'like', '%' . $s . "%")
+                ->orWhere('mail', 'like', '%' . $s . "%")
+                ->get();
+        } else {
+            // 無条件
+            $items = DB::table('people')->get();
+        }
+
+        // テンプレートファイルに渡すデータ（連想配列）
+        $data = [
+            'msg' => '登録されている会員一覧です。',
+            // peopleから読み込んだレコードをmembersの連想配列の中身とする
+            'members' => $items,
+        ];
+        // return view('hello.index', $data);
+
+        // リダイレクトでルート名を呼び出し
+        return redirect()->route('back_plan');
+    }
+
+
+    public function planShow(Request $request)
+    {
+        $items = Plan::all();
+        return view('cms.back_plan', ['items' => $items]);
+    }
+
+    public function planEntry(Request $request)
+    {
+        return view('cms.back_plan_new');
+    }
+
+    public function planCreate(Request $request)
+    {
+        $this->validate($request, plan::$rules);
+        $plan = new plan();
+        $form = $request->all();
+        unset($form['_token']);
+        $plan->fill($form)->save();
+        return redirect()->route('planshow');
+    }
+
+    public function planEdit(Request $request)
+    {
+        $plan = Plan::find($request->id);
+        return view('cms.back_plan_edit', ['form' => $plan]);
+    }
+
+    public function planUpdate(Request $request)
+    {
+        $this->validate($request, Plan::$rules);
+        $plan = Plan::find($request->id);
+        $form = $request->all();
+        unset($form['_token']);
+        $plan->fill($form)->save();
+        return redirect()->route('planshow');
+    }
+
+    public function planDelete(Request $request)
+    {
+        $plan = Plan::find($request->id);
+        return view('cms.back_plan_edit', ['form' => $plan]);
+    }
+
+    public function planRemove(Request $request)
+    {
+        Plan::find($request->id)->delete();
+        // return redirect('plan_show');
+        return redirect()->route('planshow');
+    }
+
 }
+
+
