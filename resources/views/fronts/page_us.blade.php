50c37043c3b95bb863f05c5231a7637de70930fa
panamochi <dalkur3@gmail.com>
2023-02-16 00:48:23 +0900

    Update page_us.blade.php
    レイアウトファイルを継承した。
    
    木田

--- resources/views/fronts/page_us.blade.php
+++ resources/views/fronts/page_us.blade.php
@@ -1,17 +1,43 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.layout_front')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>私たちについて</title>
-</head>
+@section('description', 'ページのデスクリプション')
 
-<body>
-    <h1>私たちについて</h1>
+@section('keywords', 'キーワード1,キーワード2・・・')
 
+@section('title', '私たちについて')
 
-</body>
+{{-- 該当ページのCSS --}}
+@section('pageCss')
 
-</html>
+@endsection
+
+{{-- メイン --}}
+@section('content')
+
+    <section class="titlesection">
+        <div class="titlesection__box">
+            <h1 class="pagetitle">私たちについて</h1>
+        </div>
+    </section>
+
+    <section class="section">
+
+        <h2 class="section__box--title ">私たちとは</h2>
+
+        <div>
+            <h3>サブタイトル</h3>
+            <div>
+                <p>テキストテキストテキストテキストテキストテキストテキスト</p>
+            </div>
+        </div>
+    </section>
+
+    <div id="page_top" class="flex">
+        <a href="#"><img src="./assets/images/fish hook.png" alt="釣り針" class="topbutton__hook"></a>
+    </div>
+@endsection
+
+{{-- 該当ページ専用JS --}}
+@section('pageJs')
+
+@endsection
