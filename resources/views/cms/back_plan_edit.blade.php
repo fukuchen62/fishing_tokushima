7789ed1020f78004877f67d05d05c858a3cbbae6
shimohachi-ryotaro <124222136+shimohachi-ryotaro@users.noreply.github.com>
2023-02-15 16:36:41 +0900

    編集
    編集
    
    下八

--- resources/views/cms/back_plan_edit.blade.php
+++ resources/views/cms/back_plan_edit.blade.php
@@ -7,7 +7,7 @@
     @parent
     {{-- 以下はshowのところにはめ込む --}}
     <ul>
-        <li><a href= "http://localhost/fishing_tokushima/public/admin/planshow" >HOME</a></li>
+        <li><a href="http://localhost/fishing_tokushima/public/admin/planshow">HOME</a></li>
         <li><a href="http://localhost/fishing_tokushima/public/admin/planentry">新規登録</a></li>
         <li>その他</li>
     </ul>
@@ -15,62 +15,84 @@
 
 @section('content')
     @if (count($errors) > 0)
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 
-<!-- <form action="admin/newsedit" method="post"> -->
-<form method="post" action="{{ route('planedit') }}">
-    <table>
-        @csrf
-        <input type="hidden" name="id" value="{{$form->id}}">
-        <tr><th>プラン名: </th><td><input type="text" name="name"
-            value="{{$form->name}}" required></td></tr>
-        <tr><th>タイトル: </th><td><input type="text" name="title"
-            value="{{$form->title}}" required></td></tr>
-        <tr><th>概要: </th><td><input type="text" name="overview"
-            value="{{$form->overview}}" required></td></tr>
+    <!-- <form action="admin/newsedit" method="post"> -->
+    <form method="post" action="{{ route('planedit') }}">
+        <table>
+            @csrf
+            <input type="hidden" name="id" value="{{ $form->id }}">
+            <tr>
+                <th>タイトル: </th>
+                <td><input type="text" name="title" value="{{ $form->title }}" required></td>
+            </tr>
             <tr>
                 <th>難易度:</th>
                 <td>
-                    <textarea name="content" id="content" cols="50" rows="5" required> {{$form->content}} </textarea>
+                    <textarea name="number" id="number" cols="50" rows="5" required> {{ $form->number }} </textarea>
                 </td>
             </tr>
-            <tr><th>地域・市町村:</th><td>
-                <input type="number" name="eyecatch" value="{{$form->eyecatch}}">
-            </td></tr>
-            <tr><th>釣りスポットID:</th><td>
-                <input type="number" name="is_show" value="{{$form->is_show}}">
-            </td></tr>
-            <tr><th>魚ID:</th><td>
-                <input type="number" name="is_show" value="{{$form->is_show}}">
-            </td></tr>
+            <tr>
+                <th>概要: </th>
+                <td><input type="overview" name="overview" value="{{ $form->overview }}" required></td>
+            </tr>
+            <tr>
+                <th>地域・市町村:</th>
+                <td>
+                    <input type="number" name="city_id" value="{{ $form->city_id }}">
+                </td>
+            </tr>
+            <tr>
+                <th>釣りスポットID:</th>
+                <td>
+                    <input type="number" name="spot_id" value="{{ $form->spot_id }}">
+                </td>
+            </tr>
+            <tr>
+                <th>魚ID:</th>
+                <td>
+                    <input type="number" name="fish_id" value="{{ $form->fish_id }}">
+                </td>
+            </tr>
             <tr>
                 <th>釣り方のフロー:</th>
                 <td>
-                    <textarea name="content" id="content" cols="50" rows="5" required> {{$form->content}} </textarea>
+                    <textarea name="flow" id="flow" cols="50" rows="5" required> {{ $form->flow }} </textarea>
                 </td>
             </tr>
-            <tr><th>アイキャッチ:</th><td>
-                <input type="text" name="eyecatch" value="{{$form->eyecatch}}">
-            </td></tr>
-            <tr><th>サムネイル:</th><td>
-                <input type="text" name="is_show" value="{{$form->is_show}}">
-            </td></tr>
-            <tr><th>表示フラグ:</th><td>
-                <input type="boolean" name="is_show" value="{{$form->is_show}}">
-            </td></tr>
-        <tr><th></th><td><input type="submit"value="修正">
+            <tr>
+                <th>アイキャッチ:</th>
+                <td>
+                    <input type="text" name="eye_catch" value="{{ $form->eye_catch }}">
+                </td>
+            </tr>
+            <tr>
+                <th>サムネイル:</th>
+                <td>
+                    <input type="text" name="thumbnail" value="{{ $form->thumbnail }}">
+                </td>
+            </tr>
+            <tr>
+                <th>表示フラグ:</th>
+                <td>
+                    <input type="boolean" name="is_show" value="{{ $form->is_show }}">
+                </td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit"value="修正">
 
-    </table>
+        </table>
     </form>
 @endsection
 
 @section('footer')
-copyright 2020 tuyano.
+    copyright 2020 tuyano.
 @endsection
