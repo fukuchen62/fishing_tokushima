8926e3f75475975fc3dd29385cd1aa466c6d74d8
shimohachi-ryotaro <124222136+shimohachi-ryotaro@users.noreply.github.com>
2023-02-15 16:12:32 +0900

    編集
    編集
    
    下八

--- resources/views/cms/back_plan_new.blade.php
+++ resources/views/cms/back_plan_new.blade.php
@@ -7,7 +7,7 @@
     @parent
     {{-- 以下はshowのところにはめ込む --}}
     <ul>
-        <li><a href= "http://localhost/fishing_tokushima/public/admin/planshow" >HOME</a></li>
+        <li><a href="http://localhost/fishing_tokushima/public/admin/planshow">HOME</a></li>
         <li><a href="http://localhost/fishing_tokushima/public/admin/planentry">新規登録</a></li>
         <li>その他</li>
     </ul>
@@ -15,45 +15,66 @@
 
 @section('content')
     <!-- <form action="admin/newsentry" method="post"> -->
-        <form method="post" action="{{ route('planentry') }}">
-    <table>
-        @csrf
-        <!-- <tr><th>プラン名: </th><td><input type="text"
-            name="name" required></td></tr> -->
-        <tr><th>タイトル: </th><td><input type="text"
-            name="title" required></td></tr>
-        <tr><th>概要: </th><td><input type="text"
-            name="overview" required></td></tr>
+    <form method="post" action="{{ route('planentry') }}">
+        <table>
+            @csrf
+            <!-- <tr><th>プラン名: </th><td><input type="text"
+                            name="name" required></td></tr> -->
             <tr>
-                <th>難易度：</th>
+                <th>タイトル: </th>
+                <td><input type="text" name="title" required></td>
+            </tr>
+            <tr>
+                <th>難易度: </th>
+                <td><input type="number" name="level" required></td>
+            </tr>
+            <tr>
+                <th>概要：</th>
                 <td>
-                    <textarea name="level" id="content" cols="50" rows="5" required></textarea>
+                    <textarea name="overview" id="overview" cols="50" rows="5" required></textarea>
                 </td>
             </tr>
-            <tr><th>地域・市町村: </th><td><input type="text"
-                name="city_id" ></td></tr>
-            <tr><th>釣りスポット: </th><td><input type="boolean"
-                name="spot_id" value="1"></td></tr>
-            <tr><th>魚: </th><td><input type="boolean"
-                name="fish_id" value="1"></td></tr>
-                <tr><th>釣り方のフロー: </th><td><input type="boolean"
-                name="flow" value="1"></td></tr>
-                <tr><th>アイキャッチ: </th><td><input type="text"
-                name="eye_catch" ></td></tr>
-            <tr><th>サムネイル: </th><td><input type="boolean"
-                name="thumbnail" value="1"></td></tr>
-            <tr><th>表示フラグ: </th><td><input type="boolean"
-                name="is_show" value="1"></td></tr>
-            
+            <tr>
+                <th>地域・市町村: </th>
+                <td><input type="number" name="city_id" required></td>
+            </tr>
+            <tr>
+                <th>釣りスポット: </th>
+                <td><input type="number" name="spot_id" required></td>
+            </tr>
+            <tr>
+                <th>魚: </th>
+                <td><input type="number" name="fish_id" required></td>
+            </tr>
+            <tr>
+                <th>釣り方のフロー：</th>
+                <td>
+                    <textarea name="flow" id="flow" cols="50" rows="5"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <th>アイキャッチ: </th>
+                <td><input type="text" name="eye_catch"></td>
+            </tr>
+            <tr>
+                <th>サムネイル: </th>
+                <td><input type="text" name="thumbnail"></td>
+            </tr>
+            <tr>
+                <th>表示フラグ: </th>
+                <td><input type="boolean" name="is_show"></td>
+            </tr>
 
-        <tr><th></th><td><input type="submit"
-            value="送信"></td></tr>
-    </table>
+
+            <tr>
+                <th></th>
+                <td><input type="submit" value="送信"></td>
+            </tr>
+        </table>
     </form>
 @endsection
 
 
 @section('footer')
-copyright 2020 tuyano.
+    copyright 2020 tuyano.
 @endsection
-
