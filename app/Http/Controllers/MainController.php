1a278d9f4a811e419a7cb477c82a059e2ed2414c
toshikawa-yurika <124222099+toshikawa-yurika@users.noreply.github.com>
2023-02-15 18:21:52 +0900

    cookieの修正
    cookieの修正

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -43,16 +43,27 @@ class MainController extends Controller
      */
     public function myPageShow(Request $request)
     {
-        $msg = 'cookieはありません';
+        $msg1 = 'cookieはありません';
+        $msg2 = 'cookieはありません';
+        $msg3 = 'cookieはありません';
 
-        if ($request->hasCookie('msg')) {
-            $msg = 'cookie:' . $request->cookie('fish_id');
-        } else {
-            $msg = 'cookieはありません';
+        if ($request->hasCookie('fish_id')) {
+            $msg1 = 'cookie:' . $request->cookie('fish_id');
         }
 
+        if ($request->hasCookie('spot_id')) {
+            $msg2 = 'cookie:' . $request->cookie('spot_id');
+        }
+
+        if ($request->hasCookie('plan_id')) {
+            $msg3 = 'cookie:' . $request->cookie('plan_id');
+        }
+
+
         $data = [
-            'msg' => $msg,
+            'msg1' => $msg1,
+            'msg2' => $msg2,
+            'msg3' => $msg3,
         ];
 
         return view('fronts.mypage', $data);
@@ -73,15 +84,36 @@ class MainController extends Controller
 
         // $this->validate($request, $validate_rule);
 
-        $id = $request->id;
+        $fish_id = $request->fish_id;
+        $spot_id = $request->spot_id;
+        $plan_id = $request->plan_id;
+
 
         $data = [
-            'msg' => '「' . $id . '」をcookieに保存しました。'
+            'msg1' => 'fish_id:「' . $fish_id . '」をcookieに保存しました。',
+            'msg2' => 'spot_id:「' . $spot_id . '」をcookieに保存しました。',
+            'msg3' => 'plan_id:「' . $plan_id . '」をcookieに保存しました。',
         ];
 
         $response = response()->view('fronts.mypage', $data);
 
-        $response->withCookie('fish_id', $id, 100);
+
+        setcookie("fish_id[0]", $fish_id);
+        setcookie("fish_id[1]", $spot_id);
+        setcookie("fish_id[2]", $plan_id);
+
+        // $cookie = [
+        //     'key' => 'fish_id',
+        //     'value' => $fish_id,
+        //     'minit' => 100,
+        // ];
+
+        $val = $_COOKIE['cookie'];
+
+        // $response->withCookie($cookie);
+
+        // ->withCookie('spot_id', $spot_id, 100)
+        // ->withCookie('plan_id', $plan_id, 100);
 
         return $response;
 
--- resources/views/fronts/mypage.blade.php
+++ resources/views/fronts/mypage.blade.php
@@ -8,23 +8,29 @@
 </head>
 <body>
 
-<p>{{ $msg }}</p>
+<p>{{ $msg1 }}</p>
+<p>{{ $msg2 }}</p>
+<p>{{ $msg3 }}</p>
+
 
 
     <form action="{{ route('mypage')}}" method="post">
         <table>
             @csrf
 
-            @if ($errors->has('msg'))
+            {{-- @if ($errors->has('msg'))
             <tr>
                 <th>ERROR</th>
                 <td>{{ $errors -> fist('msg') }}</td>
             </tr>
-            @endif
+            @endif --}}
 
             <tr>
                 <th>MESSAGE:</th>
-                <td><input type="submit" name="id" value="1" id=""></td>
+                <td><input type="submit" name="fish_id" value="1" id=""></td>
+                <td><input type="submit" name="spot_id" value="2" id=""></td>
+                <td><input type="submit" name="plan_id" value="3" id=""></td>
+
                 {{-- <td><input type="submit" value="送信"></td> --}}
             </tr>
         </table>
