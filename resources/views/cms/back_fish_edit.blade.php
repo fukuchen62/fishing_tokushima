21f942374bde2d5506785e51619b06e0de9ef39b
noboru-ogawa <no.ogawa560211@gmail.com>
2023-02-15 13:35:57 +0900

    修正
    修正
    
    小川

--- resources/views/cms/back_fish_edit.blade.php
+++ resources/views/cms/back_fish_edit.blade.php
@@ -7,8 +7,8 @@
     @parent
     {{-- 以下はshowのところにはめ込む --}}
     <ul>
-        <li><a href= "http://localhost/fishing_tokushima_bkp0210/public/admin/fishshow" >HOME</a></li>
-        <li><a href="http://localhost/fishing_tokushima_bkp0210/public/admin/fishentry">新規登録</a></li>
+        <li><a href= "http://localhost/fishing_tokushima/public/admin/fishshow" >HOME</a></li>
+        <li><a href="http://localhost/fishing_tokushima/public/admin/fishentry">新規登録</a></li>
         <li>その他</li>
     </ul>
 @endsection
