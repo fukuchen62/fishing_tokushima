2d95f2b4b43f5c4ead95a8535f39f67f7953a649
hamazi-n <hamazitime112233@gmail.com>
2023-02-16 12:38:25 +0900

    変更
    変更
    
    濱田直輝

---
+++ resources/views/components/front_spots_card.blade.php
@@ -0,0 +1,9 @@
+<!-- カードの構造 -->
+<div>
+    <img src="{{ $spot_src }}" alt="{{ $spot_alt }}">
+    <div>
+        <h3>{{ $spot_title }}</h3>
+        <p>{{ $spot_text }}</p>
+    </div>
+</div>
+<!-- /カードの構造 -->
