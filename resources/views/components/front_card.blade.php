9fc8a08601347c65428dd784d622beed1f1e3607
woodfield-Tw <dalkur3@gmail.com>
2023-02-16 15:32:29 +0900

    カード型のコンポーネントの更新
    フロントが更新したものに合わせた。
    
    木田

--- resources/views/components/front_card.blade.php
+++ resources/views/components/front_card.blade.php
@@ -1,9 +1,11 @@
 <!-- カードの構造 -->
-<div>
-    <img src="{{ $card_src }}" alt="{{ $card_alt }}">
-    <div>
-        <h3>{{ $card_title }}</h3>
-        <p>{{ $card_text }}</p>
-    </div>
-</div>
+<li class="card__p">
+    <a class="card card__skin" href="{{ $card_link }}">
+        <img class="card__img" src="{{ $card_src }}" alt="{{ $card_alt }}">
+        <div class="card__textbox">
+            <h3 class="card__title">{{ $card_title }}</h3>
+            <p class="card__description">{{ $card_text }}</p>
+        </div>
+    </a>
+</li>
 <!-- /カードの構造 -->
