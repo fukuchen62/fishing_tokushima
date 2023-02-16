8a3f1fa31b3284b2214ed87836722216f2949c25
OkadaNaho <mottu0401@gmail.com>
2023-02-16 16:22:13 +0900

    fish関連のファイルの編集
    fishのbladeファイル・componentファイルを編集
    
    岡田

--- resources/views/fronts/fish_list.blade.php
+++ resources/views/fronts/fish_list.blade.php
@@ -1,3 +1,48 @@
+@extends('layouts.layout_front')
+
+@section('description', 'ページのデスクリプション')
+
+@section('keywords', 'キーワード1,キーワード2・・・')
+
+@section('title', '徳島の魚一覧')
+
+{{-- 該当ページのCSS --}}
+@section('pageCss')
+
+@endsection
+
+{{-- メイン --}}
+@section('content')
+
+    <section class="titlesection">
+        <div class="titlesection__box">
+            <h1 class="pagetitle">魚一覧</h1>
+        </div>
+    </section>
+
+    <section class="section">
+
+        <h2 class="section__box--title ">第一セクション</h2>
+
+        <div>
+            <h3>サブタイトル</h3>
+            <div>
+                <p>テキストテキストテキストテキストテキストテキストテキスト</p>
+            </div>
+        </div>
+    </section>
+
+    <div id="page_top" class="flex">
+        <a href="#"><img src="./assets/images/fish hook.png" alt="釣り針" class="topbutton__hook"></a>
+    </div>
+@endsection
+
+{{-- 該当ページ専用JS --}}
+@section('pageJs')
+
+@endsection
+
+
 <!DOCTYPE html>
 <html lang="ja">
 
