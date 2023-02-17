290126af8f3036dbf8ee3537a53f3ce4d6d4447e
OkadaNaho <mottu0401@gmail.com>
2023-02-17 09:53:40 +0900

    Update index.blade.php
    index.blade.phpを変更
    
    岡田

--- resources/views/fronts/index.blade.php
+++ resources/views/fronts/index.blade.php
@@ -21,144 +21,102 @@
 @section('content')
 
     <section class="firstsection">
+        <h2>入門知識</h2>
+        <div>
+            <a href="{{ route('knowledgelist', ['category_id' => 1]) }}">基礎知識</a>
+        </div>
+        <div>
+            <a href="{{ route('knowledgelist', ['category_id' => 2]) }}">道具・餌</a>
+        </div>
+        <div>
+            <a href="{{ route('knowledgelist', ['category_id' => 3]) }}">注意点</a>
+        </div>
+        <div>
+            <a href="{{ route('knowledgelist', ['category_id' => 4]) }}">その他</a>
+        </div>
 
-        <h2 class="section__box--title ">あああああ</h2>
-
-
-
-    </section>
-    <div class="firstsection__bottom"></div>
-
-    <section>
         <h2>釣りスポット</h2>
-        <div>東部</div>
-        <!--botan -->
-        <div>南部</div>
-        <!--botan -->
+        <div>
+            <a href="{{ route('spotslist', ['city_id' => 1]) }}">東部</a>
+        </div>
+        <div>
+            <a href="{{ route('spotslist', ['city_id' => 2]) }}">南部</a>
+        </div>
     </section>
+
+    <div class="firstsection__bottom"></div>
 
     <section>
         <h2>体験プラン</h2>
         <div>
-            <div>プラン名</div>
-            <div>概要</div>
-            <div>画像</div>
+            <a href="{{ route('planinfo', ['id' => 1]) }}">アジをサビキ釣り</a>
         </div>
-
         <div>
-            <div>プラン名</div>
-            <div>概要</div>
-            <div>画像</div>
+            <a href="{{ route('planinfo', ['id' => 2]) }}">ウキ釣りでアジを釣る</a>
         </div>
-
         <div>
-            <div>プラン名</div>
-            <div>概要</div>
-            <div>画像</div>
+            <a href="{{ route('planinfo', ['id' => 1]) }}">アジをサビキ釣り</a>
         </div>
-        <a href=""> もっと見る</a>
+        <div>
+            <a href="{{ route('planlist') }}">もっとみる</a>
+        </div>
     </section>
 
     <section>
         <h2>徳島で釣れる魚</h2>
         <div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 1]) }}">アジ</a>
             </div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 2]) }}">イワシ</a>
             </div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 3]) }}">アジ</a>
             </div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 1]) }}">アジ</a>
             </div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 2]) }}">イワシ</a>
             </div>
         </div>
         <div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 3]) }}">アジ</a>
             </div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 1]) }}">アジ</a>
             </div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 2]) }}">イワシ</a>
             </div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 3]) }}">アジ</a>
             </div>
             <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
-            </div>
-        </div>
-        <div>
-            <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
-            </div>
-            <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
-            </div>
-            <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
-            </div>
-            <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
-            </div>
-            <div>
-                <div>吹き出し（魚の名前）</div>
-                <div>画像</div>
+                <a href="{{ route('fishinfo', ['id' => 1]) }}">アジ</a>
             </div>
         </div>
     </section>
 
     <section>
-        <h2>infomation</h2>
+        <h2>Infomation</h2>
         <div>
-            <div>
-                <div>
-                    日付
-                </div>
-                <div><a href="">タイトル</a></div>
-            </div>
-            <div>
-                <div>
-                    日付
-                </div>
-                <div><a href="">タイトル</a></div>
-            </div>
-            <div>
-                <div>
-                    日付
-                </div>
-                <div><a href="">タイトル</a></div>
-            </div>
-            <div>
-                <div>
-                    日付
-                </div>
-                <div><a href="">タイトル</a></div>
-            </div>
+            @foreach ($items as $key => $item)
+                <a href="{{ route('newsinfo', ['id' => $items[$key]]) }}">
+                    <ul>
+                        <li>{{ $item->created_at }}</li>
+                        <li>{{ $item->title }}</li>
+                    </ul>
+                </a>
+            @endforeach
         </div>
-        <a href="">もっと見る</a>
+        <div>
+            <a href="{{ route('news') }}">もっとみる</a>
+        </div>
     </section>
+
     <div id="page_top" class="flex">
         <!-- 余裕があれば魚が釣られるアニメーションを追加 -->
         <!-- <img src=".//assets/images/fish_yellow.png" alt="黄色魚" class="topbutton__fish"> -->
