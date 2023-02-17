0ec6625040ec70e2e54134bac2622d4386ad0ca2
hamazi-n <hamazitime112233@gmail.com>
2023-02-17 12:09:50 +0900

    spot
    spot
    
    濱田直輝

--- resources/views/cms/back_spots_edit.blade.php
+++ resources/views/cms/back_spots_edit.blade.php
@@ -23,89 +23,88 @@
         </div>
     @endif
 
-    <!-- <form action="admin/newsedit" method="post"> -->
     <form method="post" action="{{ route('spotsedit') }}">
         <table>
             @csrf
-            <input type="hidden" name="id" value="{{ $form->id }}">
+            <input type="hidden" name="id" value="{{ $spot->id }}">
             <tr>
                 <th>概要: </th>
-                <td><input type="text" name="overview" value="{{ $form->overview }}"></td>
+                <td><input type="text" name="overview" value="{{ $spot->overview }}"></td>
             </tr>
             <tr>
                 <th>画像1: </th>
-                <td><input type="text" name="img1" value="{{ $form->img1 }}"></td>
+                <td><input type="text" name="img1" value="{{ $spot->img1 }}"></td>
             </tr>
             <tr>
                 <th>画像2: </th>
-                <td><input type="text" name="img2" value="{{ $form->img2 }}"></td>
+                <td><input type="text" name="img2" value="{{ $spot->img2 }}"></td>
             </tr>
             <tr>
                 <th>画像3:</th>
                 <td>
-                    <input type="text" name="img3" value="{{ $form->img4 }}">
+                    <input type="text" name="img3" value="{{ $spot->img4 }}">
                 </td>
             </tr>
             <tr>
                 <th>画像4:</th>
                 <td>
-                    <input type="boolean" name="img4" value="{{ $form->img4 }}">
+                    <input type="boolean" name="img4" value="{{ $spot->img4 }}">
                 </td>
             </tr>
             <tr>
                 <th>画像5: </th>
-                <td><input type="text" name="img5" value="{{ $form->img5 }}"></td>
+                <td><input type="text" name="img5" value="{{ $spot->img5 }}"></td>
             </tr>
             <tr>
                 <th>魚ID: </th>
-                <td><input type="text" name="fish_id" value="{{ $form->fish_id }}"></td>
+                <td><input type="text" name="fish_id" value="{{ $spot->fish_id }}"></td>
             </tr>
             <tr>
                 <th>金額: </th>
-                <td><input type="text" name="mony" value="{{ $form->mony }}"></td>
+                <td><input type="text" name="mony" value="{{ $spot->mony }}"></td>
             </tr>
             <tr>
                 <th>ショップID:</th>
                 <td>
-                    <input type="boolean" name="shop_id" value="{{ $form->shop_id }}">
+                    <input type="boolean" name="shop_id" value="{{ $spot->shop_id }}">
                 </td>
             </tr>
             <tr>
                 <th>体験プランID: </th>
-                <td><input type="text" name="plan_id" value="{{ $form->plan_id }}"></td>
+                <td><input type="text" name="plan_id" value="{{ $spot->plan_id }}"></td>
             </tr>
             <tr>
                 <th>避難場所ID: </th>
-                <td><input type="text" name="evacuation_id" value="{{ $form->evacuation_id }}"></td>
+                <td><input type="text" name="evacuation_id" value="{{ $spot->evacuation_id }}"></td>
             </tr>
             <tr>
                 <th>駐車場ID: </th>
-                <td><input type="text" name="parking_id" value="{{ $form->parking_id }}"></td>
+                <td><input type="text" name="parking_id" value="{{ $spot->parking_id }}"></td>
             </tr>
             <tr>
                 <th>病院ID:</th>
                 <td>
-                    <input type="text" name="hospital_id" value="{{ $form->hospital_id }}">
+                    <input type="text" name="hospital_id" value="{{ $spot->hospital_id }}">
                 </td>
             </tr>
             <tr>
                 <th>スーパーマーケットID:</th>
                 <td>
-                    <input type="boolean" name="supermarket_id" value="{{ $form->supermarket_id }}">
+                    <input type="boolean" name="supermarket_id" value="{{ $spot->supermarket_id }}">
                 </td>
             </tr>
             <tr>
                 <th>飲食店ID: </th>
-                <td><input type="text" name="restaurant_id" value="{{ $form->restaurant_id }}"></td>
+                <td><input type="text" name="restaurant_id" value="{{ $spot->restaurant_id }}"></td>
             </tr>
             <tr>
                 <th>その他ID: </th>
-                <td><input type="text" name="other_id" value="{{ $form->other_id }}"></td>
+                <td><input type="text" name="other_id" value="{{ $spot->other_id }}"></td>
             </tr>
             <tr>
                 <th>表示フラグ:</th>
                 <td>
-                    <input type="boolean" name="is_show" value="{{ $form->is_show }}">
+                    <input type="boolean" name="is_show" value="{{ $spot->is_show }}">
                 </td>
             </tr>
             <tr>
