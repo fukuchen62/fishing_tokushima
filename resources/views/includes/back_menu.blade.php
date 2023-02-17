f59e41b6ee4ba6130f3fdc7a237efe719db38c47
fukuchen62 <fukuchen62@gmail.com>
2023-02-17 11:48:13 +0900

    Update back_menu.blade.php
    管理画面のメニューを修正した
    
    福島

--- resources/views/includes/back_menu.blade.php
+++ resources/views/includes/back_menu.blade.php
@@ -2,11 +2,11 @@
     <ul class="menu-box">
         <li>メニュー</li>
         <li><a href="{{ route('admintop') }}">Dashboard</a></li>
-        <li><a href="{{ route('fishlist') }}">魚</a></li>
-        <li><a href="{{ route('spotslist') }}">スポット</a></li>
-        <li><a href="{{ route('shopslist') }}">釣具屋</a></li>
-        <li><a href="{{ route('planlist') }}">体験プラン</a></li>
+        <li><a href="{{ route('fishshow') }}">魚</a></li>
+        <li><a href="{{ route('spotsshow') }}">スポット</a></li>
+        <li><a href="{{ route('shopsshow') }}">釣具屋</a></li>
+        <li><a href="{{ route('planshow') }}">体験プラン</a></li>
         <li><a href="{{ route('newsshow') }}">ニュース</a></li>
-        <li><a href="{{ route('knowledgelist') }}">入門知識</a></li>
+        <li><a href="{{ route('knowledgeshow') }}">入門知識</a></li>
     </ul>
 </section>
