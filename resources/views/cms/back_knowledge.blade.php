c2eece415a73ae2c4570408a06dd1959616257c6
OkadaNaho <mottu0401@gmail.com>
2023-02-17 12:05:50 +0900

    back_knowlegdeのbladeファイル編集
    back_knowlegdeのbladeファイル(3つ)編集
    
    岡田

--- resources/views/cms/back_knowledge.blade.php
+++ resources/views/cms/back_knowledge.blade.php
@@ -7,8 +7,8 @@
     @parent
     {{-- 以下はshowのところにはめ込む --}}
     <ul>
-        <li><a href= "http://localhost/fishing_tokushima/public/admin/knowledgeshow" >HOME</a></li>
-        <li><a href= "http://localhost/fishing_tokushima/public/admin/knowledgeentry">新規登録</a></li>
+        <li><a href="./knowledgeshow">HOME</a></li>
+        <li><a href="./knowledgeentry">新規登録</a></li>
 
     </ul>
 @endsection
@@ -25,19 +25,25 @@
 
 @section('content')
     <table border="1">
-    <tr><th>ID</th><th>カテゴリーID</th><th>タイトル</th><th>概要</th><th>修正</th></tr>
-    @foreach ($knowledgelist as $item)
         <tr>
-            <td>{{$item->id}}</td>
-            <td>{{$item->category_id}}</td>
-            <td>{{$item->title}}</td>
-            <td>{{$item->overview}}</td>
-            <td><a href="{{route('knowledgeedit',['id'=>$item->id])}}">編集</a></td>
+            <th>ID</th>
+            <th>カテゴリーID</th>
+            <th>タイトル</th>
+            <th>概要</th>
+            <th>修正</th>
         </tr>
-    @endforeach
+        @foreach ($knowledgelist as $item)
+            <tr>
+                <td>{{ $item->id }}</td>
+                <td>{{ $item->category_id }}</td>
+                <td>{{ $item->title }}</td>
+                <td>{{ $item->overview }}</td>
+                <td><a href="{{ route('knowledgeedit', ['id' => $item->id]) }}">編集</a></td>
+            </tr>
+        @endforeach
     </table>
 @endsection
 
 @section('footer')
-copyright 2023 tuttoku.
+    copyright 2023 tuttoku.
 @endsection
