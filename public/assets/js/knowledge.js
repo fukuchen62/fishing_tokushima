7dc986d8fa7589ba0fff362bd81790017c960dd0
Hhamada1 <124231052+Hhamada1@users.noreply.github.com>
2023-02-21 17:11:32 +0900

    Update knowledge.js
    knowledge.jsを更新しました。
    
    濱田大貴

--- public/assets/js/knowledge.js
+++ public/assets/js/knowledge.js
@@ -1,10 +1,12 @@
 $(function() {
   $('.tab').on('click', function() {
     $('.tab, .panel').removeClass('active');
-
     $(this).addClass('active');
-
     var index = $('.tab').index(this);
     $('.panel').eq(index).addClass('active');
   });
 });
+
+$(window).on('load', function () {
+  $('.tab:first-child').addClass('active');
+})
