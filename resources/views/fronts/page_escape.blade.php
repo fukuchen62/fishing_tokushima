a80537cf2005ddc29ceaac5cae9442bfd60f1e20
noboru-ogawa <no.ogawa560211@gmail.com>
2023-02-21 17:52:48 +0900

    修正
    プライバシーポリシーの追加
    
    小川

--- resources/views/fronts/page_escape.blade.php
+++ resources/views/fronts/page_escape.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="ja">
 
 <head>
@@ -6,10 +6,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>避難場所一覧</title>
-</head>
+</head> --}}
+
+@extends('layouts.layout_front')
+
+@section('description', 'プライバシーポリシーページ表示')
+
+@section('keywords', 'キーワード1,キーワード2・・・')
+
+@section('title', '避難場所一覧')
+
+{{-- 該当ページのCSS --}}
+@section('pageCss')
+
+@endsection
 
 <body>
-    <h1>避難場所一覧</h1>
+
+    @section('content')
+
+    <section class="titlesection">
+        <div class="titlesection__box">
+            <h1 class="pagetitle">ページタイトル</h1>
+        </div>
+    </section>
+
+    <section class="section">
+
+        <h2 class="section__box--title ">避難場所一覧</h2>
+
+        <table>
+            <tr>
+                <th>避難場所一覧</th>
+            </tr>
+            @foreach($evacuations as $item)
+            <tr>
+                <td>{{$item->getData()}}</td>
+            </tr>
+            @endforeach
+        </table>
+    </section>
+
+    <div id="page_top" class="flex">
+        <a href="#"><img src="{{ asset('assets/images/fish hook.png') }}" alt="釣り針" class="topbutton__hook"></a>
+    </div>
+@endsection
+
+{{-- 該当ページ専用JS --}}
+@section('pageJs')
+
+@endsection
+
+    {{-- <h1>避難場所一覧</h1>
     <table>
         <tr>
             <th>避難場所一覧</th>
@@ -19,7 +67,7 @@
             <td>{{$item->getData()}}</td>
         </tr>
         @endforeach
-    </table>
+    </table> --}}
 </body>
 
 </html>
