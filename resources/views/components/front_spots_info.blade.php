86ae411fa807c197b9b844f2b3ff7e05c33421c0
hamazi-n <hamazitime112233@gmail.com>
2023-02-21 12:54:01 +0900

    変更
    変更
    
    濱田直輝

---
+++ resources/views/components/front_spots_info.blade.php
@@ -0,0 +1,11 @@
+<!-- カードの構造 -->
+<li class="card__p">
+    <a class="card card__skin" href="{{ $spot_link }}">
+        <img class="card__img" src="{{ $spot_src }}" alt="{{ $spot_alt }}">
+        <div class="card__textbox">
+            <h3 class="card__title">{{ $spot_title }}</h3>
+            <p class="card__description">{{ $spot_text }}</p>
+        </div>
+    </a>
+</li>
+<!-- /カードの構造 -->
--- resources/views/fronts/spots_info.blade.php
+++ resources/views/fronts/spots_info.blade.php
@@ -149,6 +149,163 @@
         <img class="shell" src="" alt="">
     </div>
 
+
+    <div class="container shallow expand">
+        <div class="column">
+            <div class="spotinfo__images p__lr">
+                <!-- メイン画像 -->
+                <div class="spotinfo__img main-img js-main-img">
+                    <img class="spotinfo__img" src="{{ $item->img1 }}"
+                        alt="プレビュー">
+                </div>
+                <!-- サムネイル画像 -->
+                <ul class="sub-img js-sub-img">
+                    <!-- 選択されている画像の枠線を変更 -->
+                    <li class="spotinfo__img">
+                        <img src="{{ $item->img1 }}"
+                            alt="スポット写真1">
+                    </li>
+                    <li class="spotinfo__img">
+                        <img src="{{ $item->img2 }}"
+                            alt="スポット写真2">
+                    </li>
+                    <li class="spotinfo__img">
+                        <img src="{{ $item->img3 }}"
+                            alt="スポット写真3">
+                    </li>
+                    <li class="spotinfo__img">
+                        <img src="{{ $item->img4 }}"
+                            alt="スポット写真4">
+                    </li>
+                    <li class="spotinfo__img">
+                        <img src="{{ $item->img5 }}"
+                            alt="スポット写真5">
+                    </li>
+                </ul>
+            </div>
+            <!-- お気に入りボタン -->
+            <div>
+                <a href="" id="" class="favorite">
+                    <span>お気に入りに登録する</span>
+                    <img src="../assets/images/svg/favorite.svg" alt="">
+                </a>
+            </div>
+
+            <div>
+                <a href="" id="" class="favorite favorite__in">
+                    <span>お気に入りに登録済み</span>
+                    <img src="../assets/images/svg/favorite__in.svg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="column">
+            <!-- スポット概要 -->
+            <div class="spotinfo__discription p__lr p__b">
+                <div class="spotinfo__dis--textbox">
+                    <p class="spotinfo__dis--text">{{ $item->overview }}</p>
+                </div>
+            </div>
+            <!-- 詳細情報table -->
+            <section class="spotinfo__table p__lr">
+                <h3 class="table__title spotinfo__table--title">詳細情報</h3>
+                <table class="table m__b">
+                    <tr class="table__tr">
+                        <td class="table__subtitle">住所</td>
+                        <td>{{ $item->spot_address }}</td>
+                    </tr>
+                    <tr class="table__tr">
+                        <td class="table__subtitle">駐車場</td>
+                        <td>{{ $item->parking_id }}</td>
+                    </tr>
+                    <tr class="table__tr">
+                        <td class="table__subtitle">釣り方</td>
+                        <td>{{ $fish->method }}</td>
+                    </tr>
+                    <tr class="table__tr">
+                        <td class="table__subtitle">トイレ</td>
+                        <td>{{ $item->toilet }}</td>
+                    </tr>
+                    <tr class="table__tr">
+                        <td class="table__subtitle">バーベキュー</td>
+                        <td>{{ $item->bbq }}</td>
+                    </tr>
+                    <tr class="table__tr">
+                        <td class="table__subtitle">避難場所</td>
+                        <td>{{ $item->evacuation_id }}</td>
+                    </tr>
+                </table>
+            </section>
+        </div>
+    </div>
+    <!-- 狙える魚 -->
+    <div class="sea">
+        <section class="spotinfo__fish p__lr">
+            <h3 class="section__box--title spotinfo__sectiontitle--fish">狙える魚</h3>
+            <div class="spotinfo__fishes">
+                @foreach ($spots as $item)
+            @component('components.front_spots_info')
+
+                @slot('fish_id')
+                    {{ $item->fish_id }}
+                @endslot
+
+                @slot('fish_name')
+                    {{ $item->spots->fish_name }}
+                @endslot
+            @endcomponent
+        @endforeach
+            </div>
+        </section>
+
+        <!-- googleマップ -->
+        <section class="spotinfo__iframe p__lr">
+            <iframe class="googlemap"
+                src="{{ $item->iframe_url }}"
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
+    </div>
+    <!-- 貝殻イラスト -->
+    <div class="spotinfo__shell">
+        <img class="shell" src="../assets/images/shell-1.png" alt="">
+        <img class="shell" src="../assets/images/shell-2.png" alt="">
+        <img class="shell" src="../assets/images/shell-3.png" alt="">
+    </div>
+    <!-- 近くのスポットcard表示3つ -->
+    <section class="p__lr">
+        <h3 class="section__box--title spotinfo__sectiontitle">近くのスポット</h3>
+        <ul class="card__area">
+            <li class="card__p">
+                <a class="card card__skin" href="">
+                    <img class="card__img" src="https://placehold.jp/320x240.png" alt="">
+                    <div class="card__textbox">
+                        <h3 class="card__title">タイトルタイトル</h3>
+                        <p class="card__description">説明説明説明説明説明説明</p>
+                    </div>
+                </a>
+            </li>
+            <li class="card__p">
+                <a class="card card__skin" href="">
+                    <img class="card__img" src="https://placehold.jp/320x240.png" alt="">
+                    <div class="card__textbox">
+                        <h3 class="card__title">タイトルタイトル</h3>
+                        <p class="card__description">説明説明説明説明説明説明</p>
+                    </div>
+                </a>
+            </li>
+            <li class="card__p">
+                <a class="card card__skin" href="">
+                    <img class="card__img" src="https://placehold.jp/320x240.png" alt="">
+                    <div class="card__textbox">
+                        <h3 class="card__title">タイトルタイトル</h3>
+                        <p class="card__description">説明説明説明説明説明説明</p>
+                    </div>
+                </a>
+            </li>
+        </ul>
+
+        <a class="btn" href="{{ route('spotslist') }}">スポット一覧</a>
+    </section>
+
     <div id="page_top" class="flex">
         <a href="#"><img src="{{ asset('assets/images/fish hook.png') }}" alt="釣り針" class="topbutton__hook"></a>
     </div>
