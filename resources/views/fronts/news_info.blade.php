1299716dbf8c78cc569d5772751c1732db9c333d
shimohachi-ryotaro <124222136+shimohachi-ryotaro@users.noreply.github.com>
2023-02-22 11:22:21 +0900

    編集
    編集
    
    下八

--- resources/views/fronts/news_info.blade.php
+++ resources/views/fronts/news_info.blade.php
@@ -1,3 +1,118 @@
+@extends('layouts.layout_front')
+
+@section('description', 'ページのデスクリプション')
+
+@section('keywords', 'キーワード1,キーワード2・・・')
+
+@section('title', 'information一覧')
+
+@section('pageCss')
+    <link rel="stylesheet" href="../assets/css/news_list.css">
+@endsection
+
+@section('key_visual')
+    キービジュアル
+@endsection
+
+<div class="mainarea flex expand">
+
+    <main class="main-section expand">
+        <div>
+            <div class="news__bottom--up expand"></div>
+        </div>
+        <section class="firstsection expand p__lr">
+
+            <div class="news__section expand p__lr">
+                <div class="news__area">
+                    <h3 class="section__box--title">記事タイトル</h3>
+                    <p class="news__area--data">2023.2.15</p>
+                    <p class="news__area--description">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                    <img src="https://placehold.jp/559x419.png" alt="画像">
+                    <p class="news__area--description">
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+    <aside class="sidemenu ">
+        <div class="sidemenu__topimg"></div>
+        <div class="sidemenu__box">
+            <h2 class="sidemenu__title">カテゴリ</h2>
+            <div class=" sidemenu__box--block ">
+                <a class="sidemenu__discription" href="./news_list.html">全て</a>
+                <div class="sidemenu__border"></div>
+                <a class="sidemenu__discription" href="./news_list.html">更新</a>
+                <div class="sidemenu__border"></div>
+                <a class="sidemenu__discription" href="./news_list.html">体験</a>
+                <div class="sidemenu__border"></div>
+                <a class="sidemenu__discription" href="./news_list.html">イベント</a>
+                <div class="sidemenu__border"></div>
+            </div>
+        </div>
+        <div class="sidemenu__bottomimg"></div>
+    </aside>
+</div>
+<div>
+    <div class="news__bottom--up expand"></div>
+</div>
+
+
+
+<aside class="sidemenu__pagebottom  p__lr">
+
+    <h2 class="sidemenu__title">カテゴリ</h2>
+
+    <a class="sidemenu__discription" href="./news_list.html">全て</a>
+    <div class="sidemenu__border"></div>
+    <a class="sidemenu__discription" href="./news_list.html">更新</a>
+    <div class="sidemenu__border"></div>
+    <a class="sidemenu__discription" href="./news_list.html">体験</a>
+    <div class="sidemenu__border"></div>
+    <a class="sidemenu__discription" href="./news_list.html">イベント</a>
+    <div class="sidemenu__border"></div>
+
+</aside>
+
+
+
+<footer class="footer  expand">
+    <div class="footer__box  flex">
+        <ul>
+            <li>入門知識</li>
+            <li>徳島の魚</li>
+            <li>体験プラン</li>
+            <li>釣具店</li>
+            <li>避難場所</li>
+        </ul>
+        <ul>
+            <li>釣りスポット</li>
+            <li>東部</li>
+            <li>南部</li>
+        </ul>
+        <ul>
+            <li>マイページ</li>
+            <li>プライバシーポリシー</li>
+        </ul>
+    </div>
+    <ul class="sns flex">
+        <li>twitter</li>
+        <li>fasebook</li>
+        <li>instaglam</li>
+    </ul>
+    <p>copylight@turitoku.com</p>
+</footer>
+
+</body>
+
+</html>
+
+
+
+
+
+
 {{-- テストデータ --}}
 
 {{-- <!DOCTYPE html>
