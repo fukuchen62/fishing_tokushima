5cb82149e484d038404611314cce805568b75a18
OkadaNaho <mottu0401@gmail.com>
2023-02-24 17:09:46 +0900

    トップページ・fish_list編集
    トップページ・fish_list編集
    
    岡田

--- resources/views/components/front_index_card.blade.php
+++ resources/views/components/front_index_card.blade.php
@@ -1,10 +1,12 @@
 {{-- カードの構造 --}}
 {{-- toppageの入門知識 --}}
-<div class="imgbox">
-    <a href="{{ $card_link }}">
-        <img class="knowledge_img" src="{{ $card_src }}" alt="{{ $card_alt }}">
-    </a>
-</div>
+<a href="{{ $card_link }}" class="knowledge__imgbox">
+    <img class="knowledge_img" src="{{ $card_src }}" alt="{{ $card_alt }}">
+    <div class="hover-mask">
+        <p>基礎知識</p>
+    </div>
+</a>
+
 <div class="knowledge__text--block">
     <h3>{{ $card_title }}</h3>
     <p class="knowledge__text--box">{{ $card_text }}</p>
