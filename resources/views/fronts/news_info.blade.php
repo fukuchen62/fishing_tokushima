72f7632b677b1a4a33d3cd231b3d7a09304ac286
toshikawa-yurika <124222099+toshikawa-yurika@users.noreply.github.com>
2023-02-27 16:12:29 +0900

    Update news_info.blade.php
    news_info修正
    
    歳川

--- resources/views/fronts/news_info.blade.php
+++ resources/views/fronts/news_info.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layout_front_with_aside')
+@extends('layouts.layout_front')
 
 @section('description', 'ページのデスクリプション')
 
@@ -15,48 +15,76 @@
 @endsection
 
 @section('content')
-    <div>
-        <div class="news__bottom--up expand"></div>
-    </div>
-    <section class="firstsection expand p__lr">
 
-        <div class="news__section expand p__lr">
-            <div class="news__area">
-                <h3 class="section__box--title">{{ $news->title }}</h3>
-
-                @php
-                    $ts = strtotime($news->created_at);
-                @endphp
-                <p class="news__area--data">
-                    {{ date('Y.m.d', $ts) }}
-                </p>
-
-                <img src="{{ asset('storage/images') }}/{{ $news->eyecatch }}" alt="{{ $news->eyecatch }}">
-                @php
-                    echo $news->content;
-                @endphp
-
-
+    <div class="mainarea flex expand">
+        <div class="main-section expand">
+            <div>
+                <div class="news__bottom--up expand"></div>
             </div>
+            <section class="firstsection expand p__lr">
+
+                <div class="news__section expand p__lr">
+                    <div class="news__area">
+                        <h3 class="section__box--title">{{ $news->title }}</h3>
+
+                        @php
+                        $ts = strtotime($news->created_at);
+                        @endphp
+                        <p class="news__area--data">
+                            {{ date('Y.m.d', $ts) }}
+                        </p>
+
+                        @if ($news->eyecatch!="")
+                            <img src="{{ asset('storage/images') }}/{{ $news->eyecatch }}" alt="{{ $news->eyecatch }}">
+                        @else
+                            <img src="https://placehold.jp/559x419.png" alt="">
+                        @endif
+
+                        @php
+                            echo $news->content;
+                        @endphp
+                    </div>
+                </div>
+
+            </section>
+
         </div>
-
-    </section>
-@endsection
-
-@section('bottom')
+        <aside class="sidemenu ">
+            <div class="sidemenu__topimg"></div>
+            <div class="sidemenu__box">
+                <h2 class="sidemenu__title">カテゴリ</h2>
+                <div class=" sidemenu__box--block ">
+                    <a class="sidemenu__discription" href="{{ route('news') }}">全て</a>
+                    <div class="sidemenu__border"></div>
+                    <a class="sidemenu__discription" href="{{ route('news', ['name' => '更新']) }}">更新</a>
+                    <div class="sidemenu__border"></div>
+                    <a class="sidemenu__discription" href="{{ route('news', ['name' => '体験']) }}">体験</a>
+                    <div class="sidemenu__border"></div>
+                    <a class="sidemenu__discription" href="{{ route('news', ['name' => 'イベント']) }}">イベント</a>
+                    <div class="sidemenu__border"></div>
+                </div>
+            </div>
+            <div class="sidemenu__bottomimg"></div>
+        </aside>
+    </div>
     <div>
         <div class="news__bottom--up expand"></div>
     </div>
-@endsection
 
 
-@section('sidemenu')
-    <a class="sidemenu__discription" href="{{ route('news') }}">全て</a>
-    <div class="sidemenu__border"></div>
-    <a class="sidemenu__discription" href="{{ route('news', ['name' => '更新']) }}">更新</a>
-    <div class="sidemenu__border"></div>
-    <a class="sidemenu__discription" href="{{ route('news', ['name' => '体験']) }}">体験</a>
-    <div class="sidemenu__border"></div>
-    <a class="sidemenu__discription" href="{{ route('news', ['name' => 'イベント']) }}">イベント</a>
-    <div class="sidemenu__border"></div>
+
+    <aside class="sidemenu__pagebottom  p__lr">
+
+        <h2 class="sidemenu__title">カテゴリ</h2>
+
+        <a class="sidemenu__discription" href="{{ route('news') }}">全て</a>
+        <div class="sidemenu__border"></div>
+        <a class="sidemenu__discription" href="{{ route('news', ['name' => '更新']) }}">更新</a>
+        <div class="sidemenu__border"></div>
+        <a class="sidemenu__discription" href="{{ route('news', ['name' => '体験']) }}">体験</a>
+        <div class="sidemenu__border"></div>
+        <a class="sidemenu__discription" href="{{ route('news', ['name' => 'イベント']) }}">イベント</a>
+        <div class="sidemenu__border"></div>
+
+    </aside>
 @endsection
