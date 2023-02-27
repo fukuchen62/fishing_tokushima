e45b2d1519ac8d7ff808703ca6d13d4864c42528
toshikawa-yurika <124222099+toshikawa-yurika@users.noreply.github.com>
2023-02-27 14:10:33 +0900

    Update news_info.blade.php
    news_info修正
    
    歳川

--- resources/views/fronts/news_info.blade.php
+++ resources/views/fronts/news_info.blade.php
@@ -7,11 +7,11 @@
 @section('title', 'information')
 
 @section('pageCss')
-    <link rel="stylesheet" href="../assets/css/news_list.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/news_list.css') }}">
 @endsection
 
 @section('key_visual')
-    キービジュアル
+    <img class="sub-keyvisual" src="{{ asset('assets/images/news_keyvisual.jpg') }}" alt="news_keyvisual.jpg">
 @endsection
 
 @section('content')
@@ -53,50 +53,3 @@
     <a class="sidemenu__discription" href="{{ route('news', ['name' => 'イベント']) }}">イベント</a>
     <div class="sidemenu__border"></div>
 @endsection
-
-
-{{-- テストデータ --}}
-
-{{-- <!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ニュース詳細</title>
-</head>
-
-<body>
-
-    <h1>ニュース詳細</h1>
-
-    <a href="{{ route('news') }}">すべて</a>
-    <a href="{{ route('news', ['name' => '更新']) }}">更新</a>
-    <a href="{{ route('news', ['name' => '体験']) }}">体験</a>
-    <a href="{{ route('news', ['name' => 'イベント']) }}">イベント</a>
-
-    <table border="1">
-        <tr>
-            <th>記事タイトル&nbsp;</th>
-            <th>更新日時&nbsp;</th>
-            <th>カテゴリー名&nbsp;</th>
-            <th>概要&nbsp;</th>
-            <th>アイキャッチ画像&nbsp;</th>
-            <th>内容&nbsp;</th>
-        </tr>
-        <tr>
-            <td>{{ $news->title }}</td>
-            <td>{{ $news->created_at }}</td>
-            <td>{{ $news->name }}</td>
-            <td>{{ $news->content }}</td>
-            <td>{{ $news->eyecatch }}</td>
-            <td>{{ $news->content }}</td>
-        </tr>
-    </table>
-
-</body>
-</html> --}}
-
-
-{{-- テストデータ --}}
