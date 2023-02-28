10417be655265762d23529826efb6c948e789c7d
toshikawa-yurika <124222099+toshikawa-yurika@users.noreply.github.com>
2023-02-28 11:36:24 +0900

    タブの切り替え部分修正
    タブの切り替え部分修正
    
    歳川

--- public/assets/js/knowledge.js
+++ public/assets/js/knowledge.js
@@ -1,4 +1,6 @@
-$(function() {
+"use strict";
+
+$(function () {
   $('.tab').on('click', function() {
     $('.tab, .panel').removeClass('active');
     $(this).addClass('active');
@@ -8,14 +10,30 @@ $(function() {
 });
 
 $(window).on('load', function () {
-  $('.tab_item:first-child').addClass('active')
+  $('.tab_item:first-child').addClass('active');
+
+  // カテゴリ１を選択
+  // $('.active').removeClass('active');
+  // $('category1').addClass('active');
+
+  // カテゴリ２を選択
+  // $('.active').removeClass('active');
+  // $('category2').addClass('active');
+
+    // カテゴリ３を選択
+  // $('.active').removeClass('active');
+  // $('category3').addClass('active');
+
+    // カテゴリ４を選択
+  // $('.active').removeClass('active');
+  // $('category4').addClass('active');
 })
 
 
 // クリックしたらタブのスタイルが変わる
-$(function () {
-  $('.tab_item').on('click', function () {
-    $('.tab_item').removeClass('active');
-    $(this).addClass('active');
-  });
-});
+// $(function () {
+//   $('.tab_item').on('click', function () {
+//     $('.tab_item').removeClass('active');
+//     $(this).addClass('active');
+//   });
+// });
