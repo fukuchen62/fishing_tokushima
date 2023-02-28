cc8ace6309cd23377d0deaf98a7284040a30ec2a
toshikawa-yurika <124222099+toshikawa-yurika@users.noreply.github.com>
2023-02-28 12:11:19 +0900

    mypageのタブの部分、修正
    mypageのタブの部分、修正
    
    歳川

--- public/assets/js/mypage.js
+++ public/assets/js/mypage.js
@@ -3,13 +3,18 @@
 
 // タブの変更
 $(window).on('load', function () {
+    // デフォルト
     $('.tab_item:first-child').addClass('active');
 
-  // スポットを選択
-  // $('.active').removeClass('active');
-  // $('.spot').addClass('active');
+    const $category = $('input:hidden[name="category"]').val();
 
-  // プランを選択
-  // $('.active').removeClass('active');
-  // $('.plan').addClass('active');
+if ($category=="spot"){
+    //   スポットを選択
+    $('.active').removeClass('active');
+    $('.spot').addClass('active');
+}else if($category=="plan"){
+    //   プランを選択
+    $('.active').removeClass('active');
+    $('.plan').addClass('active');
+    }
 })
