7cb2e04387448f67f21ab7f101f955ca2f268bda
Hhamada1 <124231052+Hhamada1@users.noreply.github.com>
2023-03-02 10:14:25 +0900

    main.jsの変更
    濱田大貴

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,36 +7,33 @@ $(window).on('load resize', function () {
   if (winW <= devW) {
     $(".g-nav").addClass('slide-out');
     $(".hamburger").click(function () {
-      $(this).toggleClass('active');
-      $(".g-nav").toggleClass('slide-in');
-      $(".g-nav").toggleClass('slide-out');
-      $(".header__logo").toggleClass('none');
+    $(this).toggleClass('active');
+    $(".g-nav").toggleClass('slide-in');
+    $(".g-nav").toggleClass('slide-out');
+    $(".header__logo").toggleClass('none');
     });
   } else {
     $(".g-nav").removeClass('slide-out');
   }
+});
+$(window).resize(function(){
+    $(".g-nav").addClass('slide-out');
+    $(".hamburger").click(function () {
+    $(thishamburger).removeClass('active');
+    // $(".g-nav").toggleClass('slide-in');
+    $(".header__logo").toggleClass('none');
 });
 
 // TOPに戻るボタン スクロールしながらページ上部へ
 $('#topreturn').click(function () {
   var scroll = $(window).scrollTop(); //スクロール値を取得
   if (scroll > 0) {
-    $(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
-    $('body,html').animate({
-      scrollTop: 0
-    }, 2000, function () {//スクロールの速さ。数字が大きくなるほど遅くなる
-      $('#topreturn').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
-    });
+  $(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
+  $('body,html').animate({
+    scrollTop: 0
+  }, 2000, function () {//スクロールの速さ。数字が大きくなるほど遅くなる
+    $('#topreturn').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
+  });
   }
   return false;//リンク自体の無効化
 })
-
-// // クリックしたらタブのスタイルが変わる
-// $(function () {
-//   $('.tab_item').on('click', function () {
-//     $('.tab_item').removeClass('active');
-//     $(this).removeClass('tab_item');
-//     $(this).parent().addClass('active');
-// $(this).parent().addClass('tab_item');
-//   });
-// });
