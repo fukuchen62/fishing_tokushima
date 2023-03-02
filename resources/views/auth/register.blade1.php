782ce0c33743434a5c4dc780f501387e066cc76d
fukuchen62 <fukuchen62@gmail.com>
2023-03-02 16:02:52 +0900

    ユーザー登録機能を実装した
    ユーザー登録機能を実装した
    
    福島

--- resources/views/auth/register.blade1.php
+++ resources/views/auth/register.blade1.php
@@ -2,10 +2,8 @@
 
 @section('content')
     <div class="login-form">
-        {{-- <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card"> --}}
-        <div class="card-header">ユーザ登録</div>
+
+        <h2 class="card-header">ユーザ登録</h2>
 
         <div class="card-body">
             <form method="POST" action="{{ route('register') }}">
@@ -15,8 +13,8 @@
                     <label for="name" class="item-label">お名前</label>
 
                     <div class="input-box">
-                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror "
-                            name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <input id="name" type="text" name="name" value="{{ old('name') }}" required
+                            autocomplete="name" autofocus>
 
                         @error('name')
                             <span class="invalid-feedback" role="alert">
@@ -30,8 +28,8 @@
                     <label for="email" class="item-label">メールアドレス</label>
 
                     <div class="input-box">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                            name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <input id="email" type="email" name="email" value="{{ old('email') }}" required
+                            autocomplete="email">
 
                         @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -45,8 +43,7 @@
                     <label for="password" class="item-label">パスワード</label>
 
                     <div class="input-box">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                            name="password" required autocomplete="new-password">
+                        <input id="password" type="password" name="password" required autocomplete="new-password">
 
                         @error('password')
                             <span class="invalid-feedback" role="alert">
@@ -57,24 +54,22 @@
                 </div>
 
                 <div class="input-item mb20">
-                    <label for="password-confirm" class="item-label">パスワード確認</label>
+                    <label for="password-confirm" class="item-label">パスワード確定</label>
 
                     <div class="input-box">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
-                            required autocomplete="new-password">
+                        <input id="password-confirm" type="password" required autocomplete="new-password">
                     </div>
                 </div>
 
                 <div class="input-item mb20">
-                    <div class="item-label">
-                        <button type="submit" class="btn btn-primary">登録
+                    <div class="button-box">
+                        <button type="submit" class="btn btn-primary">
+                            登　録
                         </button>
                     </div>
                 </div>
             </form>
         </div>
-        {{-- </div>
-            </div>
-        </div> --}}
+
     </div>
 @endsection
